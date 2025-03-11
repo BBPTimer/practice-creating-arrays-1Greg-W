8241863638a7dad73cdaf67e63cce42a29bceb5d
gweseloh <greg.weseloh@gmail.com>
2025-03-11 14:09:33 -0500

    Logged arrays

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
+let groceryStoreItems = ["milk","eggs","green peppers","oatmeal","bananas"];
+let prices = [4.99,5.99,1.99];
 
+console.log(groceryStoreItems);
+console.log(prices);
+
+for(let i = 0; i < 3; i++) {
+    console.log(groceryStoreItems[i]);
+}
+
+for(let j = 0; j < 2; j++) {
+    console.log(`$${prices[j]}`);
+}
